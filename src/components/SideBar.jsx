878d0f42ca790dd1b54298313a320f9cea05f33f
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-09-19 20:07:44 -0700

    Initial Setup done

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,32 @@
+
+const SideBar = () => {
+  return (
+    <div>
+      <div className="drawer h-[100vh]  lg:drawer-open sidebar-design">
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
